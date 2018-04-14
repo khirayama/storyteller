c2278e19c83cd30a7f7026197d9c569dd665a2ed
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-04-14 12:29:51 +0900

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import { startStory } from './utils';
+import { Board } from './views/board';
+import { story } from './sample-story';
+import { BoardController } from './board-controller';
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.log(`Start app at ${(new Date()).toString()}.`);
+
+  startStory(story).then((generatedStory) => {
+    const canvasElement = window.document.querySelector('.storyteller');
+
+    const board = new Board(canvasElement, generatedStory, {
+      rulerColor: 'rgba(0, 0, 0, 0.36)',
+      padding: {
+        x: 320,
+        y: 200,
+      },
+    });
+
+    const controller = new BoardController(board);
+    controller.size(window.innerWidth, window.innerHeight);
+    controller.zoom(0.2);
+    controller.translate(80, 100);
+  });
+});
