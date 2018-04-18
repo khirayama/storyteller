d71282329080636df235d0bfdbf3e78406486117
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-04-18 15:06:27 +0900

    Add export modules

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import { Board } from './views/board';
 import { story } from './twitter-story';
 import { BoardController } from './board-controller';
 
+export {
+  startStory,
+  Board,
+  BoardController,
+};
+
 window.addEventListener('DOMContentLoaded', () => {
   console.log(`Start app at ${(new Date()).toString()}.`);
 
