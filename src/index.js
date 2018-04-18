e7a7cb800bfb01b3172845b18908e0526a192b14
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-04-18 15:33:07 +0900

    Clean up dependencies and packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { startStory } from './utils';
 import { Board } from './views/board';
-//import { story } from './sample-story';
-import { story } from './twitter-story';
 import { BoardController } from './board-controller';
 
 export {
@@ -9,26 +7,3 @@ export {
   Board,
   BoardController,
 };
-
-window.addEventListener('DOMContentLoaded', () => {
-  console.log(`Start app at ${(new Date()).toString()}.`);
-
-  startStory(story).then((generatedStory) => {
-    const canvasElement = window.document.querySelector('.storyteller');
-
-    const board = new Board(canvasElement, generatedStory, {
-      padding: {
-        x: 320,
-        y: 200,
-      },
-    });
-
-    const controller = new BoardController(board);
-    controller.size(window.innerWidth, window.innerHeight);
-    controller.fit(100, 100);
-
-    document.querySelector('.reset').addEventListener('click', () => {
-      controller.position(0, 0);
-    });
-  });
-});
